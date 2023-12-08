f0b8762a545d50a7529742aee194b7157d21f382
isnendyankp <isnendyankp@gmail.com>
2023-12-08 13:47:16 +0700

    create return with AppContext.Provider

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,11 +3,12 @@ import { createContext, useState, useContext } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-
+ return <AppContext.Provider ></AppContext.Provider>;
 };
 
 // Progress:
 // 1. Create Context
 // 2. import createContext, useState, useContext from react
 // 3. Create AppContext variable
-// 4. Create AppProvider function with children props base
+// 4. Create AppProvider function with children props base'
+// 5. create return with AppContext.Provider
