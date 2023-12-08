a43f739c06374b611d3030600d66091ecbcb978a
isnendyankp <isnendyankp@gmail.com>
2023-12-08 13:48:18 +0700

    pass children props to AppContext.Provider return

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useContext } from 'react';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
- return <AppContext.Provider ></AppContext.Provider>;
+ return <AppContext.Provider >{children}</AppContext.Provider>;
 };
 
 // Progress:
@@ -12,3 +12,4 @@ export const AppProvider = ({ children }) => {
 // 3. Create AppContext variable
 // 4. Create AppProvider function with children props base'
 // 5. create return with AppContext.Provider
+// 6. pass children props to AppContext.Provider return
