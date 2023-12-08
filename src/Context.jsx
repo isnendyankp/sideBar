cf5d83a4cd94eae718f9afa955acb05d82dadd44
isnendyankp <isnendyankp@gmail.com>
2023-12-08 13:45:33 +0700

    Create AppProvider function with children props base

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,7 +2,12 @@ import { createContext, useState, useContext } from 'react';
 
 const AppContext = createContext();
 
+export const AppProvider = ({ children }) => {
+
+};
+
 // Progress:
 // 1. Create Context
 // 2. import createContext, useState, useContext from react
 // 3. Create AppContext variable
+// 4. Create AppProvider function with children props base
