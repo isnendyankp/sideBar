0bb4df4778e6dd038ff17357dec9b2f33ce8cfbb
isnendyankp <isnendyankp@gmail.com>
2023-12-08 13:43:37 +0700

    Create AppContext variable

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,7 +1,8 @@
 import { createContext, useState, useContext } from 'react';
 
-
+const AppContext = createContext();
 
 // Progress:
 // 1. Create Context
 // 2. import createContext, useState, useContext from react
+// 3. Create AppContext variable
