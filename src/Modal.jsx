2c70c20fd7fa43d2f6fccb9c8e3e056216035261
isnendyankp <isnendyankp@gmail.com>
2023-12-13 13:23:50 +0700

    Import FaTimes icon

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import { FaTimes } from 'react-icons/fa';
 
 const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
-}
+  return <div>Modal</div>;
+};
 
-export default Modal
+export default Modal;
 
 // Progress:
 // 1. Create Modal component base
+// 2. Import FaTimes icon
