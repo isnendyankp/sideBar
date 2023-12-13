6ab1b70b19a611d51c6d478b92ff419666fdfc6e
isnendyankp <isnendyankp@gmail.com>
2023-12-13 13:28:49 +0700

    call useGlobalContext function and pass in destructured values

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,6 +2,7 @@ import { FaTimes } from 'react-icons/fa';
 import { useGlobalContext } from './context';
 
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
   return <div>Modal</div>;
 };
 
@@ -11,3 +12,4 @@ export default Modal;
 // 1. Create Modal component base
 // 2. Import FaTimes icon
 // 3. Import useGlobalContext
+// 4. call useGlobalContext function and pass in destructured values of isModalOpen & closeModal
