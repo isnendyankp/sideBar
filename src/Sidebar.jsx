59e2518256ee411aac3d0e0921c805f4b7376293
isnendyankp <isnendyankp@gmail.com>
2023-12-18 14:14:57 +0700

    import useGlobalContext

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { social, links } from './data';
 import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
@@ -15,3 +16,4 @@ export default Sidebar;
 // 3. Import social & links data from data.js file @return
 // 4. Import FaTimes icon
 // 5. call useGlobalContext function and pass in destructured values of isSidebarOpen & closeSidebar
+// 6. import useGlobalContext
