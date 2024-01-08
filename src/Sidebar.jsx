18bf91f6c75d7f222f420a1afb7556d4c7deb65a
isnendyankp <isnendyankp@gmail.com>
2024-01-08 14:47:48 +0700

    Refactor sidebar class name to show sidebar when open

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,7 +6,7 @@ import { useGlobalContext } from './context';
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className="sidebar">
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
       <div className="sidebar-header">
         <img src={logo} alt="coding addict" className="logo" />
         <button className="close-btn" onClick={closeSidebar}>
@@ -41,29 +41,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-// Progress:
-// 1. Create sidebar component base
-// 2. Import logo
-// 3. Import social & links data from data.js file @return
-// 4. Import FaTimes icon
-// 5. call useGlobalContext function and pass in destructured values of isSidebarOpen & closeSidebar
-// 6. import useGlobalContext
-// 7. add aside element with className of sidebar @return
-// 8. add div element with className of sidebar-header @return>aside
-// 9. add img element with src of logo @return>aside>div
-// 10. add alt coding addict attribute to img element @return>aside>div>img
-// 11. add className of logo to img element @return>aside>div>img
-// 12. add button element with className of close-btn @return>aside>div
-// 13. pass in FaTimes in button element @return>aside>div>button
-// 14. add onClick event to button element and pass in closeSidebar function @return>aside>div>button
-// 15. add ul element with className of links @return>aside
-// 16. add base links.map() method @return>aside>ul
-// 17. pass in links as parameter to links.map() method @return>aside>ul
-// 18. add return with li element @return>aside>ul>links
-// 19. call links & destructure id, url, text, icon from links @return>aside>ul>links>li
-// 20. add key attribute to li element and pass in id @return>aside>ul>links>li
-// 21. add a element to li element with pass in url @return>aside>ul>links>li
-// 22. pass in icon to a element @return>aside>ul>links>li>a
-// 23. pass in text to a element @return>aside>ul>links>li>a
-// 24. add ul element with className of social-links @return>aside
